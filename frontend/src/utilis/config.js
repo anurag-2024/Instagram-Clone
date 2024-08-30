fa46e8e019fab6db7502d82687c9f55c30cc11d2
anurag-2420 <anuragpatel20june@gmail.com>
2024-08-31 00:16:54 +0530

    update

--- frontend/src/utilis/config.js
+++ frontend/src/utilis/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://instagram-server-v91e.onrender.com/api";
 export const API_KEY="u08qaWyJrlxnryEgSQwbBXsjBJWwl3XFtGC9hpGlgcNkBpZ68v2BmZAel1bYGMOi";
